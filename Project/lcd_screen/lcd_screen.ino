12d37e93859329adeb313e45202ef05a6d2ff71c
khaledgg <khaber.eg@gmail.com>
2022-12-12 11:38:45 -0500

    Presentation Day

--- Project/lcd_screen/lcd_screen.ino
+++ Project/lcd_screen/lcd_screen.ino
@@ -55,6 +55,10 @@ byte breath_symb[8]={
   0b00100
 };
 
+//button press
+const int buttonPin = 2;  // the number of the pushbutton pin
+int buttonState = 0;  // variable for reading the pushbutton status
+
 void setup() {
   lcd.begin(16, 2);
   lcd.print("Initializing...");
@@ -66,27 +70,38 @@ void setup() {
   lcd.createChar(3, o2_symb);
   lcd.createChar(4, breath_symb);
   lcd.clear(); 
+
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 }
 
 
 void loop() {
   delay(1000);
   bp_baseline();
-  delay(3000);
+  wait_for_bp();
   get_baseline_stats();
   delay(1000);
-  show_stats(120,20,98);
+  show_stats(83,8,98);
   delay(3000);  
   // cal_fault();
   // delay(1000);  
   start_rec_ex();
-  delay(1000);
+  wait_for_bp();
   get_ex_stats();
-  delay(1000);
+  show_stats(103,11,97);
+  wait_for_bp();
   get_f_level();
   while (true){
     //
   } 
+}
+
+void wait_for_bp(){
+  while (digitalRead(buttonPin) != HIGH ){
+    delay(10); 
+  }
 }
 void bp_baseline(){
   lcd.clear();
@@ -100,7 +115,7 @@ void get_baseline_stats(){
   lcd.setCursor(0, 0);
   lcd.print("Getting Baseline");
 
-  for (int t = 10; t>=0; t--){
+  for (int t = 30; t>=0; t--){
     lcd.setCursor(8, 1); 
     lcd.print(" ");
     lcd.setCursor(7, 1);    
@@ -109,8 +124,10 @@ void get_baseline_stats(){
   }
   
   lcd.clear();
-  lcd.setCursor(2, 0); // column, row, starts at 0
-  lcd.print("Baseline SUCCESSFUL");
+  lcd.setCursor(0, 0); // column, row, starts at 0
+  lcd.print("Baseline");
+  lcd.setCursor(2,1);
+  lcd.print("SUCCESSFUL");
 }
 
 void show_stats(int HR, int BrPM, int O2Sat){
@@ -170,9 +187,9 @@ void cal_fault(){
 void start_rec_ex(){
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.write("Press button to start");
-  lcd.setCursor(1, 1);
-  lcd.write("recording exercise");
+  lcd.write("Press button to");
+  lcd.setCursor(0, 1);
+  lcd.write("record exercise");
 }
 
 void get_ex_stats(){
@@ -180,7 +197,7 @@ void get_ex_stats(){
   lcd.setCursor(0, 0);
   lcd.print("Getting Exercise Stats");
 
-  for (int t = 10; t>=0; t--){
+  for (int t = 30; t>=0; t--){
     lcd.setCursor(8, 1); 
     lcd.print(" ");
     lcd.setCursor(7, 1);    
@@ -200,7 +217,7 @@ void get_f_level(){
   lcd.setCursor(1, 1);
   lcd.print("Intensity Level");
 
-  delay(2000);
+  delay(3000);
 
   lcd.clear();
   lcd.setCursor(0, 0);
